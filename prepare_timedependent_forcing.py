d4136bd549010268ca6dc6fafb97e0f30455092c
Matthias Mengel <mengel@pik-potsdam.de>
2017-08-23 16:30:07 +0200

    added control file.

--- prepare_timedependent_forcing.py
+++ prepare_timedependent_forcing.py
@@ -6,11 +6,8 @@ import shutil
 data_dir = "/home/mengel/data/20170822_InitMipAntarctica"
 initmip_anomaly_file = os.path.join(data_dir,"initmip_initmip8km.nc")
 
-output_to_input_names = {
- 'effective_shelf_base_mass_flux':'shelfbmassflux',
- 'effective_shelf_base_temperature':'shelf_base_temperature',
- 'effective_climatic_mass_balance':'climatic_mass_balance',
- 'effective_ice_surface_temp':'ice_surface_temperature'}
+make_time_dependent_forcing = False
+
 
 nc_initmip = nc.Dataset(initmip_anomaly_file,"r")
 asmb = nc_initmip.variables["asmb"][:]
@@ -38,54 +35,83 @@ years_in_second_for_forcing = 31556926.
 time_factor = np.ones(100)
 time_factor[0:40] = np.linspace(0,1,40)
 
-forcing_file = os.path.join(data_dir,"initmip_forcing.nc")
-shutil.copyfile(initmip_anomaly_file,forcing_file)
-ncf = nc.Dataset(forcing_file,"a")
+## Forcing File, having explicit time dimension for climatic_mass_balance, ice_surface_temp
+## shelfbmassflux and shelfbtemp
+if make_time_dependent_forcing:
+    forcing_file = os.path.join(data_dir,"initmip_forcing.nc")
+    shutil.copyfile(initmip_anomaly_file,forcing_file)
+    ncf = nc.Dataset(forcing_file,"a")
 
-ncf.createDimension("time")
+    ncf.createDimension("time")
 
-bnds_var_name = "time_bnds"
-bnds_dim = "nb2"
-ncf.createDimension(bnds_dim, 2)
+    bnds_var_name = "time_bnds"
+    bnds_dim = "nb2"
+    ncf.createDimension(bnds_dim, 2)
 
-time_var = ncf.createVariable("time", 'float64', ("time"),)
-time_var.bounds = bnds_var_name
-time_var.units = "seconds since 0000-01-01"
-time_var.calendar = "365_day"
-time_var.axis = 'T'
-time_var[:] = tm*years_in_seconds
+    time_var = ncf.createVariable("time", 'float64', ("time"),)
+    time_var.bounds = bnds_var_name
+    time_var.units = "seconds since 0000-01-01"
+    time_var.calendar = "365_day"
+    time_var.axis = 'T'
+    time_var[:] = tm*years_in_seconds
 
-# create time bounds variable
-time_bnds_var = ncf.createVariable(bnds_var_name, 'd', dimensions=("time", bnds_dim))
-time_bnds_var[:, 0] = np.arange(startyear,endyear,1.)*years_in_seconds
-time_bnds_var[:, 1] = np.arange(startyear+1, endyear+1)*years_in_seconds
+    # create time bounds variable
+    time_bnds_var = ncf.createVariable(bnds_var_name, 'd', dimensions=("time", bnds_dim))
+    time_bnds_var[:, 0] = np.arange(startyear,endyear,1.)*years_in_seconds
+    time_bnds_var[:, 1] = np.arange(startyear+1, endyear+1)*years_in_seconds
 
+    climatic_mass_balance = ncf.createVariable("climatic_mass_balance", "float64",("time","y", "x"))
+    # convert anomaly from ice equivalent thickness to kg m-2 s-1
+    anomaly = (time_factor[:,np.newaxis,np.newaxis] * asmb *
+                    ice_density / years_in_second_for_forcing)
+    climatic_mass_balance[:] = smb_background[:] + anomaly
+    climatic_mass_balance.units = smb_background.units
 
-climatic_mass_balance = ncf.createVariable("climatic_mass_balance", "float64",("time","y", "x"))
+    # ice_surface_temp does not change from the reference run, just repeated in time
+    ice_surface_temp = ncf.createVariable("ice_surface_temp", "float64",("time","y", "x"))
+    ice_surface_temp[:] = np.tile(temp_background[:],(len(tm),1,1))
+    ice_surface_temp.units = temp_background.units
+
+    shelfbmassflux = ncf.createVariable("shelfbmassflux", "float64",("time","y", "x"))
+    # convert anomaly from ice equivalent thickness to kg m-2 year-1 as in pism file.
+    anomaly = time_factor[:,np.newaxis,np.newaxis] * abmb * ice_density
+    shelfbmassflux[:] = bmb_background[:] + anomaly
+    shelfbmassflux.units = bmb_background.units
+
+    # ice_surface_temp does not change from the reference run, just repeated in time
+    shelfbtemp = ncf.createVariable("shelfbtemp", "float64",("time","y", "x"))
+    shelfbtemp[:] = np.tile(btemp_background[:],(len(tm),1,1))
+    shelfbtemp.units = btemp_background.units
+
+    ncf.close()
+
+# Control run file, without explicit time dimension
+control_file = os.path.join(data_dir,"initmip_control.nc")
+shutil.copyfile(initmip_anomaly_file,control_file)
+ncc = nc.Dataset(control_file,"a")
+
+climatic_mass_balance = ncc.createVariable("climatic_mass_balance", "float64",("y", "x"))
 # convert anomaly from ice equivalent thickness to kg m-2 s-1
-anomaly = (time_factor[:,np.newaxis,np.newaxis] * asmb *
-                ice_density / years_in_second_for_forcing)
-climatic_mass_balance[:] = smb_background + anomaly
+climatic_mass_balance[:] = smb_background[:]
 climatic_mass_balance.units = smb_background.units
 
-# ice_surface_temp does not change from the reference run, just repeated in time
-ice_surface_temp = ncf.createVariable("ice_surface_temp", "float64",("time","y", "x"))
-ice_surface_temp[:] = np.tile(temp_background,(len(tm),1,1))
+ice_surface_temp = ncc.createVariable("ice_surface_temp", "float64",("y", "x"))
+ice_surface_temp[:] = temp_background[:]
 ice_surface_temp.units = temp_background.units
 
-
-
-shelfbmassflux = ncf.createVariable("shelfbmassflux", "float64",("time","y", "x"))
-# convert anomaly from ice equivalent thickness to kg m-2 year-1 as in pism file.
-anomaly = time_factor[:,np.newaxis,np.newaxis] * abmb * ice_density
-shelfbmassflux[:] = bmb_background + anomaly
+shelfbmassflux = ncc.createVariable("shelfbmassflux", "float64",("y", "x"))
+shelfbmassflux[:] = bmb_background[:]
 shelfbmassflux.units = bmb_background.units
 
 # ice_surface_temp does not change from the reference run, just repeated in time
-shelfbtemp = ncf.createVariable("shelfbtemp", "float64",("time","y", "x"))
-shelfbtemp[:] = np.tile(btemp_background,(len(tm),1,1))
+shelfbtemp = ncc.createVariable("shelfbtemp", "float64",("y", "x"))
+shelfbtemp[:] = btemp_background[:]
 shelfbtemp.units = btemp_background.units
+
+# del ncc.variables["abmb"]
+
+ncc.close()
 
 pism_reference_file.close()
 nc_initmip.close()
-ncf.close()
+

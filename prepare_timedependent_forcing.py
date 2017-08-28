2923606db3e8a0859a2b04bc09f0022265df9013
Matthias Mengel <mengel@pik-potsdam.de>
2017-08-28 11:16:51 +0200

    correcting the forcing timing.

--- prepare_timedependent_forcing.py
+++ prepare_timedependent_forcing.py
@@ -37,8 +37,8 @@ bmb_background = pism_reference_file.variables['effective_shelf_base_mass_flux']
 btemp_background = pism_reference_file.variables['effective_shelf_base_temperature']
 
 startyear = 2000
-endyear = 2100
-tm = np.arange(startyear,endyear,1.)
+endyear = 2105
+tm = np.arange(startyear,endyear+1,1.)
 years_in_seconds = 60*60*24*365
 
 # from Initmip
@@ -49,8 +49,8 @@ years_in_second_for_forcing = 31556926.
 ## from the INITMIP Wiki
 # SMB(t) = SMB_initialization + SMB_anomaly * (floor (t) / 40); for 0 < t < 40 in years
 # SMB(t) = SMB_initialization + SMB_anomaly * 1.0; for t > 40 years
-time_factor = np.ones(100)
-time_factor[0:40] = np.linspace(0,1,40)
+time_factor = np.ones(106)
+time_factor[0:41] = np.linspace(0,1,41)
 
 ## Forcing File, having explicit time dimension for climatic_mass_balance, ice_surface_temp
 ## shelfbmassflux and shelfbtemp
@@ -67,15 +67,15 @@ if make_time_dependent_forcing:
 
     time_var = ncf.createVariable("time", 'float64', ("time"),)
     time_var.bounds = bnds_var_name
-    time_var.units = "seconds since 0000-01-01"
+    time_var.units = "seconds since 2000-01-01"
     time_var.calendar = "365_day"
     time_var.axis = 'T'
-    time_var[:] = tm*years_in_seconds
+    time_var[:] = (tm-startyear)*years_in_seconds
 
     # create time bounds variable
     time_bnds_var = ncf.createVariable(bnds_var_name, 'd', dimensions=("time", bnds_dim))
-    time_bnds_var[:, 0] = np.arange(startyear,endyear,1.)*years_in_seconds
-    time_bnds_var[:, 1] = np.arange(startyear+1, endyear+1)*years_in_seconds
+    time_bnds_var[:, 0] = (tm-startyear)*years_in_seconds
+    time_bnds_var[:, 1] = (tm-startyear+1)*years_in_seconds
 
     climatic_mass_balance = ncf.createVariable("climatic_mass_balance", "float64",("time","y", "x"))
     # convert anomaly from ice equivalent thickness to kg m-2 s-1
